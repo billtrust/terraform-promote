fa2437acdc849ea7478e58b9927ca86382640167
Doug Kerwin <dwkerwin@gmail.com>
2019-05-11 16:43:07 -0400

    setup fix for properly swallowing missing optional pandoc

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ try:
     # in addition to pip install pypandoc, might have to: apt install -y pandoc
     import pypandoc
     long_description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError) as e:
-    print("Error converting READMD.md to rst:", str(e))
+except (OSError, IOError, ImportError) as e:
+    print("Error converting READMD.md to rst: " + str(e))
     long_description = open('README.md').read()
 
 setup(name=about['__title__'],
